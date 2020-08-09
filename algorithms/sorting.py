7ff9e7b6cf53d1cbbe0212a5c1303074bd492d7b
Javed Hussain <javed15895@gmail.com>
2020-08-09 12:24:46 +0530

    DESCENDING implementation

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,24 +1,44 @@
-# INSERTION-SORT(A)
+# INSERTION-SORT(A) --> Ascending
 # 1 for j = 2 to A.length
 # 2     key = A[j]
 # 3     // Insert A[j]  into the sorted sequence A[1..j-1].
 # 4     i = j - 1
-# 5     while i>0 and A[i] > key
+# 5     while i >= 0 and A[i] > key
 # 6         A[i+1] = A[i]
 # 7         i = i + 1
 # 8     A[i+1] = key
+
+# INSERTION-SORT(A) --> Descending
+# 1 for j = A.length -1 to 1
+# 2     key = A[j]
+# 3     // Insert A[j]  into the sorted sequence A[1..j-1].
+# 4     i = j + 1
+# 5     while i <= A.length and A[i] > key
+# 6         A[i-1] = A[i]
+# 7         i = i + 1
+# 8     A[i-1] = key
 
 class InsertionSort:
 
     def __init__(self, array):
         self.__array = array
 
-    def sort(self):
-        for j in range(1, len(self.__array)):
-            key = self.__array[j]
-            i = j - 1
-            while i >= 0 and self.__array[i] > key:
-                self.__array[i + 1] = self.__array[i]
-                i -= 1
-            self.__array[i + 1] = key
+    def sort(self, ascending=True):
+        n = len(self.__array)
+        if ascending:
+            for j in range(1, n):
+                key = self.__array[j]
+                i = j - 1
+                while i >= 0 and self.__array[i] > key:
+                    self.__array[i + 1] = self.__array[i]
+                    i -= 1
+                self.__array[i + 1] = key
+        else:
+            for j in range(n - 1, -1, -1):
+                key = self.__array[j]
+                i = j + 1
+                while i < n and self.__array[i] > key:
+                    self.__array[i - 1] = self.__array[i]
+                    i += 1
+                self.__array[i - 1] = key
         return self.__array
--- tests/sorting_test.py
+++ tests/sorting_test.py
@@ -5,9 +5,12 @@ from algorithms.sorting import InsertionSort
 
 class InsertionSortTest(unittest.TestCase):
 
-    def test_correctness(self):
-        obj = InsertionSort(array=[21, 1, 4, 34, 87, 45, 67, 0, 12, 1, 65, 45])
-        assert obj.sort(), [0, 1, 1, 4, 12, 21, 34, 45, 45, 65, 67, 87]
+    @staticmethod
+    def test_correctness():
+        ins_sort = InsertionSort(array=[3, 2, 9, 4, 5, 8, 6, 1])
+        assert ins_sort.sort(ascending=True), [1, 2, 3, 4, 5, 6, 8, 9]
+        assert ins_sort.sort(), [1, 2, 3, 4, 5, 6, 8, 9]
+        assert ins_sort.sort(ascending=False), [9, 8, 6, 5, 4, 3, 2, 1]
 
 
 if __name__ == '__main__':
