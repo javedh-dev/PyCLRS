a447842c9e4fe379703e2a0fdaf215c6e6434911
Javed Hussain <javed15895@gmail.com>
2020-09-02 11:50:11 +0530

    Binary Search

--- algorithms/searching.py
+++ algorithms/searching.py
@@ -13,3 +13,25 @@ class LinearSearch:
             if self.__array[i] == n:
                 return i
         return -1
+
+
+class BinarySearch:
+
+    def __init__(self, array):
+        self.array = sorted(array)
+
+    def search(self, n):
+        flag = True
+        high = len(self.array)-1
+        low = 0
+        while flag:
+            if high < low:
+                return -1
+            mid = (high + low) // 2
+            if self.array[mid] == n:
+                return mid
+            elif self.array[mid] > n:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
--- test/test_searching.py
+++ test/test_searching.py
@@ -1,6 +1,6 @@
 import unittest
 
-from algorithms.searching import LinearSearch
+from algorithms.searching import LinearSearch, BinarySearch
 
 
 class LinearSearchTest(unittest.TestCase):
@@ -8,6 +8,15 @@ class LinearSearchTest(unittest.TestCase):
     @staticmethod
     def test_accuracy():
         ls = LinearSearch(array=[3, 1, 2, 4, 5, 6, 7, 9, 8, 0])
+        assert ls.search(9), 7
+        assert ls.search(12), -1
+
+
+class BinarySearchTest(unittest.TestCase):
+
+    @staticmethod
+    def test_accuracy():
+        ls = BinarySearch(array=[3, 1, 2, 4, 5, 6, 7, 9, 8, 0])
         assert ls.search(9), 7
         assert ls.search(12), -1
 
